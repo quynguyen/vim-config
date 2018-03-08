c6ec128dee1fbb13b267f17b19ccd5e340e84889
quy.nguyen <quy.nguyen@gmail.com>
2018-03-07 17:23:49 -0800

    Force Commit

--- updatePlugins.sh
+++ updatePlugins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 git submodule init
-git submodule foreach git pull origin master
+git submodule foreach git pull --rebase origin master
+git submodule foreach git config --local core.excludesfile ".gitignore_submodules"
 git submodule update
