d804d0e2d54930b51da4850a00ef83385890faaa
quy.nguyen <quy.nguyen@gmail.com>
2018-03-07 17:49:45 -0800

    Force Commit

--- addPlugin.sh
+++ addPlugin.sh
@@ -7,5 +7,4 @@ path=$BUNDLE_PATH/$plugin
 
 echo "Adding plugin: $plugin, to $path"
 git submodule add $url $path
-
-
+git --work-tree=$path --git-dir=$path/.git config --local core.excludesfile ../../../.gitignore_submodules
